478957805c2336c29bd72f52e5ac8840be1d1c34
mLpenguin <trueepicmaster@gmail.com>
2026-06-17 00:01:27 -0600

    Change base

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/cosmora-health-main/',
+  base: '/website1/',
   plugins: [react(), tailwindcss()],
 })
